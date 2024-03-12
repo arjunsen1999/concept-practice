6ec7dcd60c9cef441760d26a594c7ebe228898d6
Arjun Sen <arjunsen717@gmail.com>
2024-03-12 23:11:29 +0530

    update

--- project-setup/app/page.tsx
+++ project-setup/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. At molestiae
           aut provident accusantium modi ullam! Rem, labore.
         </p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa aliquam cumque explicabo quo harum sed non dolorum alias magni quasi, enim totam itaque magnam ea eveniet quibusdam. Quis, dolorem officiis!</p>
       </div>
     </>
   )
