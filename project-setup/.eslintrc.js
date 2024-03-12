04344ddf78575b14fa0e939f424c2c66641346a8
Arjun Sen <arjunsen717@gmail.com>
2024-03-12 23:10:32 +0530

    update

---
+++ project-setup/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // Add your custom rules here
+  },
+}
